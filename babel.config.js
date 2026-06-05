11bb6072c9c66ab27fbf12620dc33ad578baca96
deepanshu-coding <deepanshusharma8174@gmail.com>
2026-06-05 11:26:48 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    '@babel/plugin-transform-flow-strip-types',
-    '@babel/plugin-transform-runtime'
-  ]
+  plugins: []
 };
