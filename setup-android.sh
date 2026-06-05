394506f99fb273c4d3c61e66d7eaf22149eaae76
deepanshu-coding <deepanshusharma8174@gmail.com>
2026-06-05 09:55:42 +0530

    Update setup-android.sh

--- setup-android.sh
+++ setup-android.sh
@@ -6,47 +6,48 @@ rm -f gradlew gradlew.bat
 rm -rf gradle/wrapper
 
 mkdir -p gradle/wrapper
-curl -L -o gradle/wrapper/gradle-wrapper.jar https://raw.githubusercontent.com/gradle/gradle/v8.5.0/gradle/wrapper/gradle-wrapper.jar
+curl -L -o gradle/wrapper/gradle-wrapper.jar https://raw.githubusercontent.com/gradle/gradle/v8.0.2/gradle/wrapper/gradle-wrapper.jar
 
 # Create gradle-wrapper.properties
-echo "distributionBase=GRADLE_USER_HOME" > gradle/wrapper/gradle-wrapper.properties
-echo "distributionPath=wrapper/dists" >> gradle/wrapper/gradle-wrapper.properties
-echo "distributionUrl=https\://services.gradle.org/distributions/gradle-8.5-bin.zip" >> gradle/wrapper/gradle-wrapper.properties
-echo "zipStoreBase=GRADLE_USER_HOME" >> gradle/wrapper/gradle-wrapper.properties
-echo "zipStorePath=wrapper/dists" >> gradle/wrapper/gradle-wrapper.properties
+cat > gradle/wrapper/gradle-wrapper.properties << 'EOL'
+distributionBase=GRADLE_USER_HOME
+distributionPath=wrapper/dists
+distributionUrl=https\://services.gradle.org/distributions/gradle-8.0.2-bin.zip
+zipStoreBase=GRADLE_USER_HOME
+zipStorePath=wrapper/dists
+EOL
 
 # Download gradlew scripts
-curl -L -o gradlew https://raw.githubusercontent.com/gradle/gradle/v8.5.0/gradlew
+curl -L -o gradlew https://raw.githubusercontent.com/gradle/gradle/v8.0.2/gradlew
 chmod +x gradlew
-curl -L -o gradlew.bat https://raw.githubusercontent.com/gradle/gradle/v8.5.0/gradlew.bat
+curl -L -o gradlew.bat https://raw.githubusercontent.com/gradle/gradle/v8.0.2/gradlew.bat
 
 cd ..
 
 # Create settings.gradle
-cat > android/settings.gradle << 'EOF'
+cat > android/settings.gradle << 'EOL'
 rootProject.name = 'DatalakeFaceLiveness'
 apply from: file("../node_modules/@react-native-community/cli-platform-android/native_modules.gradle"); applyNativeModulesSettingsGradle(settings)
 include ':app'
-includeBuild('../node_modules/@react-native/gradle-plugin')
-EOF
+EOL
 
-# Create build.gradle with FIXED kotlin version
-cat > android/build.gradle << 'EOF'
+# Create build.gradle with stable versions
+cat > android/build.gradle << 'EOL'
 buildscript {
     ext {
-        buildToolsVersion = "34.0.0"
-        minSdkVersion = 23
-        compileSdkVersion = 34
-        targetSdkVersion = 34
-        ndkVersion = "25.1.8937393"
-        kotlinVersion = "1.9.22"
+        buildToolsVersion = "33.0.0"
+        minSdkVersion = 21
+        compileSdkVersion = 33
+        targetSdkVersion = 33
+        ndkVersion = "23.1.7779620"
+        kotlinVersion = "1.7.0"
     }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.android.tools.build:gradle:7.3.1")
         classpath("com.facebook.react:react-native-gradle-plugin")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
@@ -59,6 +60,6 @@ allprojects {
         maven { url 'https://www.jitpack.io' }
     }
 }
-EOF
+EOL
 
 echo "Android setup complete!"
